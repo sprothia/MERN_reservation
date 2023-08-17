edf98edbbb64bb314463220823694ef9ed07eefc
Siddharth Prothia <79033000+sprothia@users.noreply.github.com>
2023-08-17 12:22:35 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jsonWebTok = require('jsonwebtoken')
 const mongoose = require("mongoose")
 const User = require('./models/User')
